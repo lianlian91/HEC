43d70dac6b618e894b5d09c4d5278624722009dd
lianlian91 <muangmuang955@gmail.com>
2026-08-02 11:03:06 +0630

    Update invitation card design and adding more students

--- script.js
+++ script.js
@@ -30,25 +30,85 @@
                 image: "img/shwezin.jpg"
             },
             {
+                name: "Nicholas Lian Thawn Muang",
+                diocease: "Hakha",
+                image: "img/nicholas.jpg"
+            },
+            {
+                name: "Mary Hkawn San",
+                diocese: "Myitkyina",
+                image: "img/hkawnsan.jpg"
+            },
+            {
                 name: "Theresa Seng Sawm Ing",
                 diocese: "Myitkyina",
                 image: "img/sengsawm.jpg"
             },
             {
+                name: "David Sam Sin",
+                diocease: "Lashio",
+                image: "img/david.jpg"
+            },
+            {
                 name: "Monica Htu Ring Ja",
-                diocese: "Bamaw",
+                diocese: "Banmaw",
                 image : "img/thuringja.jpg"
             },
             {
                 name: "Cecilia Kai Yin",
-                diocese: "Bamaw",
-                image : "img/kayin.jpg"
+                diocese: "Banmaw",
+                image : "img/kaiyin.jpg"
             },
-            // {
-            //     name: "Nicholas Lian Thawn Muang",
-            //     diocese: "Hakha",
-            //     image : "/img/francis.jpg"
-            // }
+            {
+                name: "Christopher Ko Ko",
+                diocese: "Loikaw",
+                image : "img/christopher.jpg"
+            },
+            {
+                name: "Elizabeth Thae Moe Khaing",
+                diocese: "Banmaw",
+                image : "img/thaemo.jpg"
+            },
+            {
+                name: "Maria Ei Jar",
+                diocese: "Lashio",
+                image: "img/eijar.jpg"
+            },
+            {
+                name: "Mary Htoo",
+                diocese: "Phekhon",
+                image: "img/kohto.jpg"
+            },
+            {
+                name: "Elizabeth Thang Awi",
+                diocese: "Mindat",
+                image: "img/thangawi.jpeg"
+            },
+            {
+                name: "Juliana Hla Ya Min Htike",
+                diocese: "Mandalay",
+                image: "img/majuice.jpg"
+            },
+            {
+                name: "Mary No San Cing",
+                diocese: "Kalay",
+                image: "img/ethan.jpg"
+            },
+            {
+                name: "Joseph Nang Zing La Mu",
+                diocese: "Banmaw",
+                image: "img/lamu.jpg"
+            },
+            {
+                name: "Martha Yadana Oo",
+                diocese: "Mawlamyine",
+                image: "img/marthaB.jpg"
+            },
+            {
+                name: "Martha Hnin Thiri Wai",
+                diocese: "Mawlamyine",
+                image: "img/marthaA.jpg"
+            }
         ];
 
         function renderGraduates() {
